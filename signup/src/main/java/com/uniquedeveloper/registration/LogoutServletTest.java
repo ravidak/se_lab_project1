27705248f4f694a5562a3d765ef5d39031eeacf3
Raviranjan kumar <101610787+ravidak@users.noreply.github.com>
2024-03-11 23:21:35 +0530

    Update LogoutServletTest.java

--- signup/src/main/java/com/uniquedeveloper/registration/LogoutServletTest.java
+++ signup/src/main/java/com/uniquedeveloper/registration/LogoutServletTest.java
@@ -17,3 +17,33 @@ import org.mockito.MockitoAnnotations;
 import com.uniquedeveloper.registration.Logout;
 
 public class LogoutServletTest {
+    
+    @Mock
+    HttpServletRequest request;
+    
+    @Mock
+    HttpServletResponse response;
+    
+    @Mock
+    HttpSession session;
+    
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
+    
+    @Test
+    public void testDoGet_Success() throws ServletException, IOException {
+        // Mock HttpSession
+        when(request.getSession()).thenReturn(session);
+        
+        // Test Logout servlet doGet method
+        new Logout().doGet(request, response);
+        
+        // Verify HttpSession invalidation
+        verify(session).invalidate();
+        
+        // Verify redirect to login.jsp
+        verify(response).sendRedirect("login.jsp");
+    }
+}

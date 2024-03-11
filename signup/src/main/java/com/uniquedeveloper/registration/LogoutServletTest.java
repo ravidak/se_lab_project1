bb616fff03197273bd1c73cc04749e66bfaaadee
Raviranjan kumar <101610787+ravidak@users.noreply.github.com>
2024-03-11 23:08:35 +0530

    Create LogoutServletTest.java

---
+++ signup/src/main/java/com/uniquedeveloper/registration/LogoutServletTest.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.uniquedeveloper.registration.Logout;
+
+public class LogoutServletTest {
